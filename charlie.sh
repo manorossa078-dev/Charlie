d9ba8b272a2304677a34e7beed953d264b2fdf88
manorossa078-dev <manorossa078@gmail.com>
2026-06-12 16:15:30 +0200

    Update to v.0.0.7

--- charlie.sh
+++ charlie.sh
@@ -3,7 +3,7 @@
 GREEN="\e[1;32m"
 RED="\e[1;31m"
 DEFAULT="\e[0m"
-VERSION="v.0.0.6"
+VERSION="v.0.0.7"
 
 echo -e "${GREEN}"
 
@@ -18,57 +18,66 @@ EOF
 echo "Charlie $VERSION"
 
 if [ -z "$1" ]; then
-	echo -e "${RED}Error. Provice a flag (-h for help)." >&2
-	exit 1
+        echo -e "${RED}Error. Provice a flag (-h for help)." >&2
+        exit 1
 elif [ "$1" == "-h" ]; then
-	echo "---------------------------------------"
-	echo "Options for Charlie:"
-	echo "-h shows this message"
-	echo "-cm changes the MAC address"
-	echo "-rm resets to original MAC address"
-	echo "-st scans a target"
-	echo "-sn scans a network"
-	echo "-sp to scan a phone"
-	echo "---------------------------------------"
+        echo "---------------------------------------"
+        echo "Options for Charlie:"
+        echo "-h shows this message"
+        echo "-u shows the usage of commands"
+        echo "-cm changes the MAC address"
+        echo "-rm resets to original MAC address"
+        echo "-st scans a target"
+        echo "-sn scans a network"
+        echo "-sp to scan a phone"
+        echo "---------------------------------------"
+elif [ "$1" == "-u" ]; then
+        echo "-----------------------------------------------------"
+        echo "Usage for -cm: use -cm and it will automatically change the MAC address"
+        echo "Usage for -rm: use -rm and it'll reset automatically the MAC address"
+        echo "Usage for -st: enter the flag and then the target IPv4"
+        echo "Usage for -sn: same as for -st"
+        echo "Usage for -sp: same goes for this, except there's an IPv6 target address"
+        echo "-----------------------------------------------------"
 elif [ "$1" == "-cm" ]; then
-	read -p "Enter an interface: " interface
-	echo "Changing MAC..."
-	sudo ip link set dev $interface down
-	sudo macchanger -m 02:AA:BB:CC:DD:EE $interface
-	sudo ip link set dev $interface up
-	echo "MAC changed!"
+        read -p "Enter an interface: " interface
+        echo "Changing MAC..."
+        sudo ip link set dev $interface down
+        sudo macchanger -m 02:AA:BB:CC:DD:EE $interface
+        sudo ip link set dev $interface up
+        echo "MAC changed!"
 elif [ "$1" == "-rm" ]; then
-	read -p "Enter an interface: " interface
-	sudo ip link set dev $interface down
-	sudo macchanger -p $interface
-	sudo ip link set dev $interface up
+        read -p "Enter an interface: " interface
+        sudo ip link set dev $interface down
+        sudo macchanger -p $interface
+        sudo ip link set dev $interface up
 elif [ "$1" == "-st" ]; then
-	if [ -z "$2" ]; then
-		echo -e "${RED}Error. Enter an IPv4 address for this flag." >&2
-		exit 1
-	else
-		echo -e "Scanning IP address $2...\e[0m"
-		sudo nmap -sV -sC -O -Pn -v $2
-		echo -e "${GREEN}Done scanning!"
-	fi
+        if [ -z "$2" ]; then
+                echo -e "${RED}Error. Enter an IPv4 address for this flag." >&2
+                exit 1
+        else
+                echo -e "Scanning IP address $2...\e[0m"
+                sudo nmap -sV -sC -O -Pn -v $2
+                echo -e "${GREEN}Done scanning!"
+        fi
 elif [ "$1" == "-sn" ]; then
-	if [ -z "$2" ]; then
-		echo -e "${RED}Error. Provide a target IPv4 address."
-	else
-		echo "Scanning network..."
-		echo -e "${DEFAULT}"
-		sudo nmap -sn -Pn $target
-		echo -e "${GREEN}Done scanning!"
-	fi
+        if [ -z "$2" ]; then
+                echo -e "${RED}Error. Provide a target IPv4 address."
+        else
+                echo "Scanning network..."
+                echo -e "${DEFAULT}"
+                sudo nmap -sn -Pn $target
+                echo -e "${GREEN}Done scanning!"
+        fi
 elif [ "$1" == "-sp" ]; then
-	if [ -z "$2" ]; then
-		echo -e "${RED}Error. Provide an IPv6 address." >&2
-		exit 1
-	else
-		echo -e "Scanning target...${DEFAULT}"
-		sudo nmap -6 -sV -sC -O -Pn -v $2
-		echo -e "${GREEN}Done scanning!"
-	fi
+        if [ -z "$2" ]; then
+                echo -e "${RED}Error. Provide an IPv6 address." >&2
+                exit 1
+        else
+                echo -e "Scanning target...${DEFAULT}"
+                sudo nmap -6 -sV -sC -O -Pn -v $2
+                echo -e "${GREEN}Done scanning!"
+        fi
 else
-	echo -e "${RED}Error. Flag not recognized." >&22
+        echo -e "${RED}Error. Flag not recognized." >&22
 fi
